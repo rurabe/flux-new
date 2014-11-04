a4b8776ccbd3d5f7585704a8e440ef35e085f341
Ryan Urabe <rurabe@gmail.com>
2014-11-04 00:40:27 -0500

    added plumber for worry free gulp watches

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var plumber = require('gulp-plumber');
 
 gulp.task('browserify', function() {
   gulp.src('src/js/app.js')
+    .pipe(plumber())
     .pipe(browserify({transform: 'reactify'}))
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public/js'));
@@ -14,24 +14,18 @@ gulp.task('browserify', function() {
 
 gulp.task('copy', function() {
   gulp.src('src/index.html')
+    .pipe(plumber())
     .pipe(gulp.dest('public'));
   gulp.src('src/img/**/*.*')
+    .pipe(plumber())
     .pipe(gulp.dest('public/img'));
 });
 
 gulp.task('sass',function(){
   gulp.src('src/css/app.sass')
+    .pipe(plumber())
     .pipe(sass({ sourceComments: 'normal' }))
     .pipe(gulp.dest('public/css'))
-    .pipe(reload({ stream: true }));
-});
-
-gulp.task('serve',function(){
-  browserSync({
-    server: {
-      baseDir: 'public'
-    }
-  });
 });
 
 gulp.task('default',['browserify', 'copy', 'sass']);
