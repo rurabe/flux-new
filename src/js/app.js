c0d98a27649a6b499f16e85f0b27d20e39f26962
Ryan Urabe <rurabe@gmail.com>
2014-11-04 01:31:06 -0500

    added react-router

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
 var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var Routes = Router.Routes;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+
 var Logo = require('./components/logo');
 
 var App = React.createClass({
   render: function(){
     return(
-      <Logo />
+      <this.props.activeRouteHandler />
     )
   }
 });
 
-React.render(<App />,document.getElementById('reactMain'));
+React.render((
+  <Routes location="history">
+    <Route path="/" handler={App}>
+      <DefaultRoute handler={Logo} />
+    </Route>
+  </Routes>
+), document.getElementById('reactMain'));
